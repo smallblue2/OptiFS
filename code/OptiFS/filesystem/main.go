bd9fe8f1ebb5b4b9d4fc0344691dcf245a569b73
zoejc <zoe.collins2@mail.dcu.ie>
2024-01-16 15:49:25 +0000

    Implemented Lookup call, as well as new node creation and inode number generation

--- code/OptiFS/filesystem/main.go
+++ code/OptiFS/filesystem/main.go
@@ -6,48 +6,102 @@ import (
 	"path/filepath"
 	"syscall"
 
-	"github.com/hanwen/go-fuse/v2/fuse"
 	"github.com/hanwen/go-fuse/v2/fs"
+	"github.com/hanwen/go-fuse/v2/fuse"
 )
 
 // Root for OptiFS
 type OptiFSRoot struct {
-    // The path to the root of the underlying file system (OptiFS is
-    // a loopback filesystem)
-    Path string
+	// The path to the root of the underlying file system (OptiFS is
+	// a loopback filesystem)
+	Path string
+
+	// device the path is on (for NFS purposes)
+	Dev uint64
+
+	// returns new Inode for lookups/creation, allowing for custom behaviour (handling certain files)
+	// if not set use an OptiFSRoot
+	NewNode func(data *OptiFSRoot, parent *fs.Inode, name string, s *syscall.Stat_t) fs.InodeEmbedder
 }
 
 // General Node for OptiFS
 type OptiFSNode struct {
-    fs.Inode
+	fs.Inode
 
-    // The root node of the filesystem
-    RootNode *OptiFSRoot
+	// The root node of the filesystem
+	RootNode *OptiFSRoot
 }
 
 // Interfaces/contracts to abide by
-var _ = (fs.NodeStatfser)((*OptiFSNode)(nil)) // StatFS
+var _ = (fs.NodeStatfser)((*OptiFSNode)(nil))  // StatFS
+var _ = (fs.InodeEmbedder)((*OptiFSNode)(nil)) // Inode
+var _ = (fs.NodeLookuper)((*OptiFSNode)(nil))  // lookup
 
 // Statfs implements statistics for the filesystem that holds this
 // Inode.
 func (n *OptiFSNode) Statfs(ctx context.Context, out *fuse.StatfsOut) syscall.Errno {
-    // As this is a loopback filesystem, we will stat the underlying filesystem.
-    var s syscall.Statfs_t = syscall.Statfs_t{}
-    err := syscall.Statfs(n.path(), &s)
-    if err != nil {
-        return fs.ToErrno(err)
-    }
-    out.FromStatfsT(&s)
-    return fs.OK
+	// As this is a loopback filesystem, we will stat the underlying filesystem.
+	var s syscall.Statfs_t = syscall.Statfs_t{}
+	err := syscall.Statfs(n.path(), &s)
+	if err != nil {
+		return fs.ToErrno(err)
+	}
+	out.FromStatfsT(&s)
+	return fs.OK
 }
 
 // Path returns the full path to the underlying file in the underlying filesystem
 func (n *OptiFSNode) path() string {
-    // Get 'n's node's path relative to OptiFS's root
-    var path string = n.Path(n.Root())
-    return filepath.Join(n.RootNode.Path, path)
+	// Get 'n's node's path relative to OptiFS's root
+	var path string = n.Path(n.Root())
+	return filepath.Join(n.RootNode.Path, path)
+}
+
+// create a new node in the system
+func (n *OptiFSRoot) newNode(parent *fs.Inode, name string, s *syscall.Stat_t) fs.InodeEmbedder {
+	// if the new node was already defined
+	if n.NewNode != nil {
+		return n.NewNode(n, parent, name, s)
+	}
+
+	// if there's no custom node, do the default creation
+	return &OptiFSNode{
+		RootNode: n,
+	}
+}
+
+// since we're using NFS, theres a chance not all Inode numbers will be unique, so we
+// calculate one using bit swapping
+func (n *OptiFSRoot) idFromStat(s *syscall.Stat_t) fs.StableAttr {
+	// swap the higher and lower bits to generate unique no
+	swapped := (uint64(s.Dev) << 32) | (uint64(s.Dev) >> 32)
+	swappedRoot := (n.Dev << 32) | (n.Dev << 32)
+
+	return fs.StableAttr{
+		Mode: uint32(s.Mode),                  // perms
+		Gen:  1,                               // generation number (determines lifetime of inode)
+		Ino:  (swapped ^ swappedRoot) ^ s.Ino, // the inode number
+	}
+}
+
+// lookup finds a file/directory based on its name
+func (n *OptiFSNode) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
+	log.Println("lookup performed")
+	fp := filepath.Join(n.path(), name) // getting the full path to the file (join name to path)
+	s := syscall.Stat_t{}               // status of a file
+	err := syscall.Lstat(fp, &s)        // gets the file attributes (also returns attrs of symbolic link)
+
+	if err != nil {
+		return nil, fs.ToErrno(err)
+	}
+
+	out.Attr.FromStat(&s)                                 // fill attrs to struct
+	nd := n.RootNode.newNode(n.EmbeddedInode(), name, &s) // create new node to rep file
+	x := n.NewInode(ctx, nd, n.RootNode.idFromStat(&s))
+
+	return x, fs.OK
 }
 
 func main() {
-    log.Println("Starting OptiFS")
+	log.Println("Starting OptiFS")
 }
