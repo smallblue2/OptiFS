ef70bfdfc7266f588474ae1d299958944e66c2f6
zoejc <zoe.collins2@mail.dcu.ie>
2024-01-22 11:30:45 +0000

    Finished implementing file locks (setlk & setlkw)

--- code/OptiFS/filesystem/file/file.go
+++ code/OptiFS/filesystem/file/file.go
@@ -19,7 +19,7 @@ type OptiFSFile struct {
 	fdesc int
 }
 
-// statuses iused commonly throughout the system, to do with locks
+// statuses used commonly throughout the system, to do with locks
 const (
 	_OFD_GETLK  = 36
 	_OFD_SETLK  = 37
@@ -28,17 +28,19 @@ const (
 
 // Interfaces for Filehandles
 var _ = (fs.FileHandle)((*OptiFSFile)(nil))
-var _ = (fs.FileReader)((*OptiFSFile)(nil)) // reading a file
-var _ = (fs.FileFsyncer)((*OptiFSFile)(nil)) // Ensuring things are written to disk
-var _ = (fs.FileFlusher)((*OptiFSFile)(nil)) // Flushes the file
+var _ = (fs.FileReader)((*OptiFSFile)(nil))    // reading a file
+var _ = (fs.FileFsyncer)((*OptiFSFile)(nil))   // Ensuring things are written to disk
+var _ = (fs.FileFlusher)((*OptiFSFile)(nil))   // Flushes the file
 var _ = (fs.FileSetattrer)((*OptiFSFile)(nil)) // Writes attributes to the file
-var _ = (fs.FileWriter)((*OptiFSFile)(nil)) // For performing write operations
+var _ = (fs.FileWriter)((*OptiFSFile)(nil))    // For performing write operations
 var _ = (fs.FileGetattrer)((*OptiFSFile)(nil)) // get attrs of a file
 var _ = (fs.FileReleaser)((*OptiFSFile)(nil))  // release (close) a file
 var _ = (fs.FileGetlker)((*OptiFSFile)(nil))   // find conflicting locks for given lock
+var _ = (fs.FileSetlker)((*OptiFSFile)(nil))   // gets a lock on a file
+var _ = (fs.FileSetlkwer)((*OptiFSFile)(nil))  // gets a lock on a file, waits for it to be ready
 
 // makes a filehandle, to give more control over operations on files in the system
-// abstract reference to files, where the state of the file (open, offsets, reading etc) 
+// abstract reference to files, where the state of the file (open, offsets, reading etc)
 // can be tracked
 func NewOptiFSFile(fdesc int) fs.FileHandle {
 	return &OptiFSFile{fdesc: fdesc}
@@ -52,31 +54,31 @@ func (f *OptiFSFile) Read(ctx context.Context, dest []byte, offset int64) (fuse.
 	defer f.mu.Unlock()
 
 	// read a specific amount of data (dest) from a specific point (offset) in the file
-    // Use the FUSE library's built-in
+	// Use the FUSE library's built-in
 	read := fuse.ReadResultFd(uintptr(f.fdesc), offset, len(dest))
 
 	return read, fs.OK
 }
 
 func (f *OptiFSFile) Fsync(ctx context.Context, flags uint32) syscall.Errno {
-    // Gain access to the mutex lock
-    f.mu.Lock()
-    defer f.mu.Unlock()
+	// Gain access to the mutex lock
+	f.mu.Lock()
+	defer f.mu.Unlock()
 
-    // Perform an Fsync on the actual file in the underlying filesystem
-    return fs.ToErrno(syscall.Fsync(f.fdesc))
+	// Perform an Fsync on the actual file in the underlying filesystem
+	return fs.ToErrno(syscall.Fsync(f.fdesc))
 }
 
 func (f *OptiFSFile) Flush(ctx context.Context) syscall.Errno {
-    // Gain access to the mutex lock
-    f.mu.Lock()
-    defer f.mu.Unlock()
+	// Gain access to the mutex lock
+	f.mu.Lock()
+	defer f.mu.Unlock()
 
-    // In order to force FUSE to flush, we will dup the filedescriptor and then close it
+	// In order to force FUSE to flush, we will dup the filedescriptor and then close it
 	tmpfd, err := syscall.Dup(f.fdesc)
-    if err != nil {
-        return fs.ToErrno(err)
-    }
+	if err != nil {
+		return fs.ToErrno(err)
+	}
 
 	return fs.ToErrno(syscall.Close(tmpfd))
 }
@@ -101,22 +103,22 @@ func (f *OptiFSFile) Getattr(ctx context.Context, out *fuse.AttrOut) syscall.Err
 }
 
 func (f *OptiFSFile) Setattr(ctx context.Context, in *fuse.SetAttrIn, out *fuse.AttrOut) syscall.Errno {
-    f.mu.Lock()
-    defer f.mu.Unlock()
+	f.mu.Lock()
+	defer f.mu.Unlock()
 
-    // Check to see if we need to change the mode
-    if mode, ok := in.GetMode(); ok {
-        // If so, change the mode
-        if err := syscall.Fchmod(f.fdesc, mode); err != nil {
-            return fs.ToErrno(err)
-        }
-    }
+	// Check to see if we need to change the mode
+	if mode, ok := in.GetMode(); ok {
+		// If so, change the mode
+		if err := syscall.Fchmod(f.fdesc, mode); err != nil {
+			return fs.ToErrno(err)
+		}
+	}
 
-    // Check to see if we need to change the UID or GID
-    uid, uok := in.GetUID()
-    gid, gok := in.GetGID()
-    // If we have a UID or GID to set
-    if uok || gok {
+	// Check to see if we need to change the UID or GID
+	uid, uok := in.GetUID()
+	gid, gok := in.GetGID()
+	// If we have a UID or GID to set
+	if uok || gok {
 		// Set their default values to -1
 		// -1 indicates that the respective value shouldn't change
 		safeUID, safeGID := -1, -1
@@ -132,7 +134,6 @@ func (f *OptiFSFile) Setattr(ctx context.Context, in *fuse.SetAttrIn, out *fuse.
 			return fs.ToErrno(err)
 		}
 	}
-
 
 	// Same thing for modification and access times
 	mtime, mok := in.GetMTime()
@@ -157,29 +158,29 @@ func (f *OptiFSFile) Setattr(ctx context.Context, in *fuse.SetAttrIn, out *fuse.
 		times[0] = fuse.UtimeToTimespec(ap)
 		times[1] = fuse.UtimeToTimespec(mp)
 
-        // BELOW LINE IS FROM `fs` package, hanwen - TODO: REFERENCE PROPERLY
-        _, _, err := syscall.Syscall6(syscall.SYS_UTIMENSAT, uintptr(f.fdesc), 0, uintptr(unsafe.Pointer(&times)), uintptr(0), 0, 0)
-        err = syscall.Errno(err)
-        if err != 0 {
-		    return fs.ToErrno(err)
+		// BELOW LINE IS FROM `fs` package, hanwen - TODO: REFERENCE PROPERLY
+		_, _, err := syscall.Syscall6(syscall.SYS_UTIMENSAT, uintptr(f.fdesc), 0, uintptr(unsafe.Pointer(&times)), uintptr(0), 0, 0)
+		err = syscall.Errno(err)
+		if err != 0 {
+			return fs.ToErrno(err)
 		}
 	}
 
-    // Check to see if we need to change the size
+	// Check to see if we need to change the size
 	if sz, ok := in.GetSize(); ok {
-        // Change the size
-        if err := fs.ToErrno(syscall.Ftruncate(f.fdesc, int64(sz))); err != 0 {
+		// Change the size
+		if err := fs.ToErrno(syscall.Ftruncate(f.fdesc, int64(sz))); err != 0 {
 			return err
 		}
 	}
 
-    return fs.OK
+	return fs.OK
 }
 
 func (f *OptiFSFile) Write(ctx context.Context, data []byte, off int64) (uint32, syscall.Errno) {
 	f.mu.Lock()
 	defer f.mu.Unlock()
-    // pwrite writes to a filedescriptor from a given offset
+	// pwrite writes to a filedescriptor from a given offset
 	numOfBytesWritten, err := syscall.Pwrite(f.fdesc, data, off)
 	return uint32(numOfBytesWritten), fs.ToErrno(err)
 }
@@ -224,4 +225,66 @@ func (f *OptiFSFile) Getlk(ctx context.Context, owner uint64, lk *fuse.FileLock,
 	err := fs.ToErrno(syscall.FcntlFlock(uintptr(f.fdesc), _OFD_GETLK, &lock))
 
 	return err
+}
+
+// gets a lock on a file, if it can't get the lock it fails
+func (f *OptiFSFile) Setlk(ctx context.Context, owner uint64, lk *fuse.FileLock, flags uint32) syscall.Errno {
+	// lock the operation, and make sure it doesnt unlock until function is exited
+	// unlocks when function is exited
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	lock := syscall.Flock_t{}
+	lk.ToFlockT(&lock) // convert the FUSE file lock to a system file lock
+
+	if lk.Typ == syscall.F_RDLCK {
+		// if we have a read lock
+		lock.Type = syscall.F_RDLCK
+	} else if lk.Typ == syscall.F_WRLCK {
+		// if we have a write lock
+		lock.Type = syscall.F_WRLCK
+	} else if lk.Typ == syscall.F_UNLCK {
+		// if we want to unlock the file
+		lock.Type = syscall.F_UNLCK
+	} else {
+		return syscall.EINVAL // invalid argument passed
+	}
+
+	// OFD_SETLK tries to get the lock on a file, if it can't, it will fail and return an error
+	// query the file lock status of the file descr
+	err := fs.ToErrno(syscall.FcntlFlock(uintptr(f.fdesc), _OFD_SETLK, &lock))
+
+	return err
+
+}
+
+// gets a lock on a file, if it can't get the lock then it waits for the lock to be obtainable
+func (f *OptiFSFile) Setlkw(ctx context.Context, owner uint64, lk *fuse.FileLock, flags uint32) syscall.Errno {
+	// lock the operation, and make sure it doesnt unlock until function is exited
+	// unlocks when function is exited
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	lock := syscall.Flock_t{}
+	lk.ToFlockT(&lock) // convert the FUSE file lock to a system file lock
+
+	if lk.Typ == syscall.F_RDLCK {
+		// if we have a read lock
+		lock.Type = syscall.F_RDLCK
+	} else if lk.Typ == syscall.F_WRLCK {
+		// if we have a write lock
+		lock.Type = syscall.F_WRLCK
+	} else if lk.Typ == syscall.F_UNLCK {
+		// if we want to unlock the file
+		lock.Type = syscall.F_UNLCK
+	} else {
+		return syscall.EINVAL // invalid argument passed
+	}
+
+	// OFD_SETLKW tries to get the lock on a file, if it can't, it will wait for it to become available
+	// query the file lock status of the file descr
+	err := fs.ToErrno(syscall.FcntlFlock(uintptr(f.fdesc), _OFD_SETLKW, &lock))
+
+	return err
+
 }
