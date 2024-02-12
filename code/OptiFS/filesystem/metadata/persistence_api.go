c28c0e39749931355cd8bac32ce1733494f2e7ba
zoejc <zoe.collins2@mail.dcu.ie>
2024-02-12 20:53:49 +0000

    Added saving of all 3 hashmaps, and put locks on these saving and retrieving functions.

--- code/OptiFS/filesystem/metadata/persistence_api.go
+++ code/OptiFS/filesystem/metadata/persistence_api.go
@@ -7,46 +7,49 @@ import (
 	"errors"
 	"log"
 	"os"
+	"sync"
 
 	"github.com/hanwen/go-fuse/v2/fs"
 )
 
+var nodeMutex sync.Mutex // lock for hashmap saving node info
+
 // Stores the content hash and reference number for keeping a node persistent between OptiFS instances
 func StoreRegFileInfo(path string, stableAttr *fs.StableAttr, mode uint32, contentHash [64]byte, refNum uint64) {
-    nodePersistenceHash[path] = &NodeInfo{stableAttr: *stableAttr, mode: mode, isDir: false, contentHash: contentHash, refNum: refNum}
+	nodePersistenceHash[path] = &NodeInfo{stableAttr: *stableAttr, mode: mode, isDir: false, contentHash: contentHash, refNum: refNum}
 }
 
 // Specifically stores a directory into the persistence hash
 func StoreDirInfo(path string, stableAttr *fs.StableAttr, mode uint32) {
-    nodePersistenceHash[path] = &NodeInfo{stableAttr: *stableAttr, mode: mode, isDir: true}
+	nodePersistenceHash[path] = &NodeInfo{stableAttr: *stableAttr, mode: mode, isDir: true}
 }
 
 // Updates node info in the persistence hash, all values except the path are optional and won't be updated if nil
 func UpdateNodeInfo(path string, isDir *bool, stableAttr *fs.StableAttr, mode *uint32, contentHash *[64]byte, refNum *uint64) {
-    log.Printf("Updating {%v}'s Persistent Data", path)
+	log.Printf("Updating {%v}'s Persistent Data", path)
 	store, ok := nodePersistenceHash[path]
 	if !ok {
-        log.Println("DOESNT EXIST!")
+		log.Println("DOESNT EXIST!")
 		return
 	}
 	if isDir != nil {
-        log.Println("Setting IsDir")
+		log.Println("Setting IsDir")
 		store.isDir = *isDir
 	}
 	if stableAttr != nil {
-        log.Println("Setting stableAttr")
+		log.Println("Setting stableAttr")
 		store.stableAttr = *stableAttr
 	}
-    if mode != nil {
-        log.Println("Setting mode")
-        store.mode = *mode
-    }
+	if mode != nil {
+		log.Println("Setting mode")
+		store.mode = *mode
+	}
 	if contentHash != nil {
-        log.Println("Setting contentHash")
+		log.Println("Setting contentHash")
 		store.contentHash = *contentHash
 	}
 	if refNum != nil {
-        log.Println("Setting refNum")
+		log.Println("Setting refNum")
 		store.refNum = *refNum
 	}
 }
@@ -71,6 +74,11 @@ func RemoveNodeInfo(path string) error {
 // Since a hashmap will be deleted when the system is restarted (stored in RAM),
 // we encode the hashmap and store it in a file saved on disk to be loaded when OptiFS starts
 func SaveMetadataMap(hashmap map[[64]byte]*MapEntry) error {
+	// lock the operation, and make sure it doesnt unlock until function is exited
+	// unlocks when function is exited
+	nodeMutex.Lock()
+	defer nodeMutex.Unlock()
+
 	log.Println("SAVING METADATA HASHMAP")
 	dest := "hashing/OptiFSRegularFileMetadataSave.gob"
 
@@ -99,6 +107,11 @@ func SaveMetadataMap(hashmap map[[64]byte]*MapEntry) error {
 // Retrieve the encoded hashmap from the file when the system restarts to maintain
 // persistence between OptiFS instances
 func RetrieveMetadataMap() error {
+	// lock the operation, and make sure it doesnt unlock until function is exited
+	// unlocks when function is exited
+	nodeMutex.Lock()
+	defer nodeMutex.Unlock()
+
 	log.Println("RETRIEVING METADATA HASHMAP")
 	dest := "hashing/OptiFSRegularFileMetadataSave.gob"
 
@@ -133,6 +146,11 @@ func PrintRegularFileMetadataHash() {
 // since a hashmap will be deleted when the system is restarted (stored in RAM)
 // we encode the hashmap and store it in a file saved on disk to be loaded when OptiFS starts
 func SaveNodePersistenceHash(hashmap map[string]*NodeInfo) error {
+	// lock the operation, and make sure it doesnt unlock until function is exited
+	// unlocks when function is exited
+	nodeMutex.Lock()
+	defer nodeMutex.Unlock()
+
 	log.Println("SAVING NODE INFO HASHMAP")
 	dest := "hashing/OptiFSNodePersistenceSave.gob"
 
@@ -160,6 +178,11 @@ func SaveNodePersistenceHash(hashmap map[string]*NodeInfo) error {
 
 // retrieve the encoded node info hashmap from the file when the system restarts
 func RetrieveNodePersistenceHash() error {
+	// lock the operation, and make sure it doesnt unlock until function is exited
+	// unlocks when function is exited
+	nodeMutex.Lock()
+	defer nodeMutex.Unlock()
+
 	log.Println("RETRIEVING NODE HASHMAP")
 	dest := "hashing/OptiFSNodePersistenceSave.gob"
 
@@ -180,4 +203,85 @@ func RetrieveNodePersistenceHash() error {
 	}
 
 	return nil
+}
+
+// save the hashmap which stores information about directories in the filesystem
+// when the filesystem restarts
+func SaveDirMetadataHash(hashmap map[string]*MapEntryMetadata) error {
+	// lock the operation, and make sure it doesnt unlock until function is exited
+	// unlocks when function is exited
+	nodeMutex.Lock()
+	defer nodeMutex.Unlock()
+
+	log.Println("SAVING DIR HASHMAP")
+	dest := "hashing/OptiFSDirMetadataSave.gob"
+
+	// create the file if it doesn't exist, truncate it if it does
+	// we assume nobody will be calling this file, as it is a very unique name
+	file, err := os.Create(dest)
+
+	if err != nil {
+		log.Println("ERROR WITH FILE - DIR HASHMAP")
+		return err
+	}
+
+	defer file.Close() // don't let the file close
+
+	encode := gob.NewEncoder(file) // set the file that we created to the encoder
+	eErr := encode.Encode(hashmap) // encode the hashmap into binary, put it in the file
+
+	if eErr != nil {
+		log.Println("ERROR WITH ENCODER - DIR HASHMAP")
+		return eErr
+	}
+
+	return nil
+
+}
+
+// retrieve the hashmap which stores information about directories in the filesystem
+// when the filesystem restarts
+func RetrieveDirMetadataHash() error {
+	// lock the operation, and make sure it doesnt unlock until function is exited
+	// unlocks when function is exited
+	nodeMutex.Lock()
+	defer nodeMutex.Unlock()
+
+	log.Println("RETRIEVING DIR HASHMAP")
+	dest := "hashing/OptiFSDirMetadataSave.gob"
+
+	file, err := os.Open(dest) // open where the hashmap was encoded
+
+	if err != nil {
+		return err
+	}
+
+	defer file.Close() // don't let the file close
+
+	decode := gob.NewDecoder(file)          // set the file that we opened to the decoder
+	dErr := decode.Decode(&dirMetadataHash) // decode the file back into the hashmap
+
+	if dErr != nil {
+		log.Println("ERROR WITH DECODER - DIR HASHMAP")
+		return dErr
+	}
+
+	return nil
+
+}
+
+// for saving the hashmaps when system is shut off
+// preserves private hashmaps
+func SavePersistantStorage() {
+	SaveNodePersistenceHash(nodePersistenceHash)
+	SaveMetadataMap(regularFileMetadataHash)
+	SaveDirMetadataHash(dirMetadataHash)
+}
+
+// for actually loading the hashmaps when the system is turned on
+// preserves private hashmaps
+func RetrievePersistantStorage() {
+	RetrieveNodePersistenceHash()
+	RetrieveMetadataMap()
+	RetrieveDirMetadataHash()
 }
